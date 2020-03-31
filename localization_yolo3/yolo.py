1169c69bc11edf20b56a7b7f2a391788ac9cb2e5
unknown <mashuai_191@163.com>
2020-03-31 22:33:18 +0800

    change font dir to absolute dir

--- localization_yolo3/yolo.py
+++ localization_yolo3/yolo.py
@@ -126,7 +126,7 @@ class YOLO(object):
 
         print('Found {} boxes for {}'.format(len(out_boxes), 'img'))
 
-        font = ImageFont.truetype(font='font/FiraMono-Medium.otf',
+        font = ImageFont.truetype(font=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'font/FiraMono-Medium.otf'),
                     size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
         thickness = (image.size[0] + image.size[1]) // 300
 
