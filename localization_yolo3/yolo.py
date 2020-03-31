6bcae515926f538c05ec01d88c64a2277738b548
unknown <mashuai_191@163.com>
2020-03-31 20:04:14 +0800

    try fix

--- localization_yolo3/yolo.py
+++ localization_yolo3/yolo.py
@@ -120,8 +120,8 @@ class YOLO(object):
             [self.boxes, self.scores, self.classes],
             feed_dict={
                 self.yolo_model.input: image_data,
-                self.input_image_shape: [image.size[1], image.size[0]],
-                K.learning_phase(): 0
+                self.input_image_shape: [image.size[1], image.size[0]]
+                #K.learning_phase(): 0
             })
 
         print('Found {} boxes for {}'.format(len(out_boxes), 'img'))
